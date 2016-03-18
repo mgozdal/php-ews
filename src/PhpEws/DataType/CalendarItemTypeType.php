b57b696b4d28e8efc5a3f4eab77be4c41ce25b15
Martin van der Poel <martin@vanderpoel.pro>
2016-03-18 18:59:03 +0100

    corrected phpdoc declarations

--- src/PhpEws/DataType/CalendarItemTypeType.php
+++ src/PhpEws/DataType/CalendarItemTypeType.php
@@ -10,28 +10,28 @@ use PhpEws\DataType;
 class CalendarItemTypeType extends DataType
 {
     /**
-     * Send to no attendees.
+     * Single
      *
      * @var string
      */
     const SINGLE = 'Single';
 
     /**
-     * Send to all attendees but do not save a copy.
+     * Occurrence
      *
      * @var string
      */
     const OCCURRENCE = 'Occurrence';
 
     /**
-     * Send to all attendees and save a copy.
+     * Exception
      *
      * @var string
      */
     const EXCEPTION = 'Exception';
 
     /**
-     * Send to only the changed attendees but do not save a copy.
+     * Recurring Master
      *
      * @var string
      */
