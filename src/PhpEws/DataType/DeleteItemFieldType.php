8c4abafdab42decdd0b8af78165b9df2ad55cbf7
Martin van der Poel <martin@vanderpoel.pro>
2016-06-22 10:53:59 +0200

    Added DeleteItemFieldType

---
+++ src/PhpEws/DataType/DeleteItemFieldType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace PhpEws\DataType;
+
+use PhpEws\DataType;
+
+/**
+ * Definition of the DeleteItemType type
+ */
+class DeleteItemFieldType extends DataType
+{
+    /**
+     * Identifies extended MAPI properties to set.
+     *
+     * @var PathToExtendedFieldType
+     */
+    public $ExtendedFieldURI;
+§
+    /**
+     * Identifies frequently referenced properties by URI.
+     *
+     * @var PathToUnindexedFieldType
+     */
+    public $FieldURI;
+
+    /**
+     * Identifies individual members of a dictionary.
+     *
+     * @var PathToIndexedFieldType
+     */
+    public $IndexedFieldURI;
+}
